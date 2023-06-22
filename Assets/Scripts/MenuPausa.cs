28bbcb1edb8e4476cad717c09a1731392a237446
TheLastArcangel <nico_cruzado1604@hotmail.com>
2023-06-22 18:59:12 -0400

    feat:Menu Pausa finalizado

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuPausa : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject wristUI;
-
+    
     public bool activeWristUI=true;
+    public Button[] botones;
+    public GameObject levelBotones;
+    private float yOffset;
 
-
+    Dictionary<string, int> levelIndexMap = new Dictionary<string, int>()
+    {
+        { "A", 0 },
+        { "B", 1 },
+        { "C", 2 },
+        { "D", 3 },
+        { "E", 4 }
+    };
     private void Start()
     {
         DisplayWristUI();
@@ -50,11 +61,67 @@ public class MenuPausa : MonoBehaviour
         SceneManager.LoadScene(levelName);
 
     }
+
+    public void GoToLevel()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        Debug.Log("Unlock " + unlockedLevel);
+        foreach (Button boton in botones)
+        {
+            boton.gameObject.SetActive(false);
+        }
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            if (i < botones.Length)
+            {
+                
+                botones[i].gameObject.SetActive(true);
+            }
+        }
+        GridLayoutGroup gridLayout = levelBotones.GetComponent<GridLayoutGroup>();
+
+        // Ajusta el valor según la ubicación deseada en el eje Y
+
+        yOffset = (unlockedLevel - 1) * 100;
+        levelBotones.transform.localPosition = new Vector3(levelBotones.transform.localPosition.x, yOffset, levelBotones.transform.localPosition.z);
+        // Ajusta el GridLayoutGroup para refrescar la disposición de los botones
+        //GridLayoutGroup gridLayout = levelBotones.GetComponent<GridLayoutGroup>();
+        //gridLayout.enabled = false;
+        //gridLayout.enabled = true;
+    }
+
+
     public void desPause()
     {
         wristUI.SetActive(false);
         activeWristUI = false;
         Time.timeScale = 1;
+    }
+    public void OpenLevel(string Level)
+    {
+
+
+        if (levelIndexMap.ContainsKey(Level))
+        {
+            string levelName = "Paso-" + Level;
+            SceneManager.LoadScene(levelName);
+        }
+    }
+
+    void ButtonsToArray()
+    {
+        int childCount = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        botones = new Button[childCount];
+        for (int i=0; i< childCount; i++)
+        {
+            botones[i] = levelBotones.transform.GetChild(i).gameObject.GetComponent<Button>();
+        }
+    }
+
+    private void Awake()
+    {
+        ButtonsToArray();
+        GoToLevel();
     }
 
 
