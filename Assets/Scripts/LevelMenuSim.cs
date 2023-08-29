4c943f573b5de7240a7ee9c22aa290ec5731c227
TheLastArcangel <nico_cruzado1604@hotmail.com>
2023-08-28 20:58:23 -0400

    acceso a lvls

---
+++ Assets/Scripts/LevelMenuSim.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class LevelMenuSim : MonoBehaviour
+{
+    private string Lvl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lvl = ""; 
+    }
+
+    public void ChangeLvl (string GoLevel)
+    {
+        
+        Lvl = GoLevel;
+        
+    }
+
+    public void GoToLevel () 
+    {
+        SceneManager.LoadScene(Lvl);
+
+    }
+}
