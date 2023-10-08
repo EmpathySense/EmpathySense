be1cb1932135485adb32afd3a963c4aed93b9e6e
Daikus <benjamin.andres.aedo@gmail.com>
2023-10-07 22:07:14 -0300

    efectos visuales graduales al terminar, y se agrego el chromatic aberration

--- Assets/Sintomas/Scripts/PostProcessing.cs
+++ Assets/Sintomas/Scripts/PostProcessing.cs
@@ -9,18 +9,32 @@ public class PostProcessing : MonoBehaviour
     private Bloom _bloom;
     private DepthOfField _depthofField;
     private LensDistortion _lensDistortion;
+    private ChromaticAberration _chromaticAberration;
 
 
-    private bool flagLens = false;
-    public AnimationCurve intensityCurveLens;
-    public float transitionDurationLens;
-    public float timerLens;
+    public float maxIntensityChromatic;
+    private bool flagChromatic = false;
+    public float transitionDurationChromatic;
+    private float timerChromatic;
+    private float timerChromaticOff = 0f;
+
+   /*  private bool flagChromaticOff = false;
+    public float timerChromaticOff; */
 
 
-    public AnimationCurve intensityCurveBloom;
+
+    public float maxIntensityBloom;
     private bool flagBloom = false;
     public float transitionDurationBloom;
-    public float timerBloom;
+    private float timerBloom;
+    private float timerBloomOff = 0f;
+
+
+    public float maxFocalLengthDepthOfField;
+    private bool flagDepthOfField = false;
+    public float transitionDurationDepthOfField;
+    private float timerDepthOfField;
+    private float timerDepthOfFieldOff = 0f;
 
     private void Start()
     {
@@ -28,7 +42,7 @@ public class PostProcessing : MonoBehaviour
         _postProcessVolume = GetComponent<PostProcessVolume>();
         _postProcessVolume.profile.TryGetSettings(out _bloom);
         _postProcessVolume.profile.TryGetSettings(out _depthofField);
-        _postProcessVolume.profile.TryGetSettings(out _lensDistortion);
+        _postProcessVolume.profile.TryGetSettings(out _chromaticAberration);
 
         //falta asignarla a la secuencia, dependiendo como se maneje
         /* DepthOfFieldOff(true);
@@ -41,16 +55,23 @@ public class PostProcessing : MonoBehaviour
     private void Update()
     {
         //Flag para entrar a la secuencia de transición
-        if(flagLens)
+        if(flagChromatic)
         {
-            if(timerLens < transitionDurationLens) 
+            if(timerChromatic < transitionDurationChromatic) 
             {
                 //Se usa la animation curve para asignar los valores a la intensidad del efecto, la curva está en el objeto asignado
-                timerLens += Time.deltaTime;
-                float t = Mathf.Clamp01(timerLens / transitionDurationLens);
-                float intensity = intensityCurveLens.Evaluate(t);
+                timerChromatic += Time.deltaTime;
+                float t = Mathf.Clamp01(timerChromatic / transitionDurationChromatic);
+                float intensity = maxIntensityChromatic * t;
             
-                _lensDistortion.intensity.value = intensity;
+                _chromaticAberration.intensity.value = intensity;
+            }
+            else
+            {
+                timerChromaticOff += Time.deltaTime;
+                float t = Mathf.Clamp01(timerChromaticOff / transitionDurationChromatic);
+                float intensity = maxIntensityChromatic * (1.0f - t);
+                _chromaticAberration.intensity.value = intensity;
             }
         }
 
@@ -61,9 +82,36 @@ public class PostProcessing : MonoBehaviour
                 Debug.Log("deberia cambiar intensity");
                 timerBloom += Time.deltaTime;
                 float t = Mathf.Clamp01(timerBloom / transitionDurationBloom);
-                float intensity = intensityCurveBloom.Evaluate(t);
+                float intensity = maxIntensityBloom * t;
             
                 _bloom.intensity.value = intensity;
+            }
+            else
+            {
+                timerBloomOff += Time.deltaTime;
+                float t = Mathf.Clamp01(timerBloomOff / transitionDurationBloom);
+                float intensity = maxIntensityBloom * (1.0f - t);
+                _bloom.intensity.value = intensity;
+                Debug.Log(intensity);
+            }
+        }
+
+        if(flagDepthOfField)
+        {
+            if(timerDepthOfField < transitionDurationDepthOfField) 
+            {
+                timerDepthOfField += Time.deltaTime;
+                float t = Mathf.Clamp01(timerDepthOfField / transitionDurationDepthOfField);
+                float intensity = maxFocalLengthDepthOfField * t;
+            
+                _depthofField.focalLength.value = intensity;
+            }
+            else
+            {
+                timerDepthOfFieldOff += Time.deltaTime;
+                float t = Mathf.Clamp01(timerDepthOfFieldOff / transitionDurationDepthOfField);
+                float intensity = maxFocalLengthDepthOfField * (1.0f - t);
+                _depthofField.focalLength.value = intensity;
             }
         }
 
@@ -77,10 +125,13 @@ public class PostProcessing : MonoBehaviour
         if (on)
         {
             _depthofField.active = true;
-            
+            float transitionDurationDepthOfField = 10f;
+            float timerDepthOfField = 0f;
+            flagDepthOfField = true;   
         }
         else
         {
+            flagDepthOfField = false;
             _depthofField.active = false;
         }
     }
@@ -100,19 +151,19 @@ public class PostProcessing : MonoBehaviour
         }
     }
 
-    public void LensDistortionOff(bool on)
+    public void ChromaticAberrationOff(bool on)
     {
         if (on)
         {
-            _lensDistortion.active = true;
-            float transitionDurationLens = 10f;
-            float timerLens = 0f;
-            flagLens = true;
+            _chromaticAberration.active = true;
+            float transitionDurationChromatic = 10f;
+            float timerChromatic = 0f;
+            flagChromatic = true;
         }
         else
         {
-            flagLens = false;
-            _lensDistortion.active = false;
+            flagChromatic = false;
+            _chromaticAberration.active = false;
         }
     }
 }
